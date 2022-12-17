ceccfc96f6859fbfeaab9b721f6b2f73282cd069
Adz946 <100402347+Adz946@users.noreply.github.com>
2022-12-18 00:57:04 +1100

    SOOOOOO MANY CHANGES
    1 - Made multiple CSS files to fit each device type's screen size
    
    2 - Used as little id/class attributes as possible
    
    3 - Made the youtube linked videos look better, and added padding
    
    4 - The entire page feels more professional now

--- functions.js
+++ functions.js
@@ -2,11 +2,9 @@ $(document).ready(function() {
     $("img").click( function () {
         $("#vid-" + this.id[4]).slideToggle("slow");
         $("#vid-" + this.id[4]).css({
-            "padding": "2%",
-            "height": "auto",
-            "max-width": "100%",
-            "margin-left": "auto",
-            "margin-right": "auto"
+            "width": "100%",
+            "height": "100%",
+            "display": "block"
         });
     })
 })
